0f4c4b1df42211366827997efd68c5f216010232
Andrii Panchenko <andrevpanchenko@gmail.com>
2023-02-16 15:21:33 +0200

    add swagger

--- backend/src/messages/messages.controller.ts
+++ backend/src/messages/messages.controller.ts
@@ -1,17 +1,23 @@
 import { Controller, Get, Post, Delete, Patch, Body } from '@nestjs/common';
 import { createMessageDto } from './dto/create-message.dto';
 import { MessagesService } from './messages.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Message } from './messages.model';
 
 @Controller('messages')
 export class MessagesController {
 
     constructor(private messagesService: MessagesService) {}
 
+    @ApiOperation({summary: 'Get all messages'})
+    @ApiResponse({status: 200, type: [Message]})
     @Get()
     getMessages(): any {
       return this.messagesService.getMessages();
     }
   
+    @ApiOperation({summary: 'Post message'})
+    @ApiResponse({status: 200, type: Message})
     @Post()
     create(@Body() messageDto: createMessageDto) {
       return this.messagesService.postMessage(messageDto);
--- backend/src/messages/messages.model.ts
+++ backend/src/messages/messages.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 interface messageCreationAttrs {
@@ -11,12 +12,15 @@ export class Message extends Model<Message, messageCreationAttrs> {
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
+    @ApiProperty({example: 'Artem', description: 'User name'})
     @Column({type: DataType.STRING, allowNull: false})
     name: string;
 
+    @ApiProperty({example: 'my_mail@gmail.com', description: 'User email'})
     @Column({type: DataType.STRING, allowNull: false})
     email: string;
 
+    @ApiProperty({example: 'Good feedback!', description: 'Feedback message from user'})
     @Column({type: DataType.STRING, allowNull: false})
     message: string;
 }
